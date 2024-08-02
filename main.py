4cf1c16db44caba69e41d853fe1c97db07f822b9
Clumsyz <73532180+Clumsyz1@users.noreply.github.com>
2024-08-02 13:00:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def on_message(message):
 async def on_ready():
     print(f'Logged in as {bot.user.name} - {bot.user.id}')
     # Set the bot's presence
-    activity = discord.Activity(type=discord.ActivityType.listening, name="fellow fellow - Proud")
+    activity = discord.Activity(type=discord.ActivityType.listening, name="Proud - fellow fellow")
     await bot.change_presence(status=discord.Status.online, activity=activity)
     
 # ///////////////////// Commands /////////////////////
